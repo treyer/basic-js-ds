ab96e70ea1881aa730d730e2995487dfeeeee6e0
treyer <nemereno_by@mail.ru>
2021-10-19 02:26:44 +0300

    feat: implement queue functionality

--- src/st-queue.js
+++ src/st-queue.js
@@ -13,21 +13,50 @@ const { NotImplementedError } = require('../extensions/index.js');
  * queue.dequeue(); // returns the top element from queue and deletes it, returns 1
  * queue.getUnderlyingList() // returns { value: 3, next: null }
  */
+
+ function ListNode(x) {
+  this.value = x;
+  this.next = null;
+}
+
 module.exports = class Queue {
 
-  getUnderlyingList() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  constructor(){
+    this.firstEl = null;
+    this.lastEl = null;
+    this.size = 0;
   }
 
-  enqueue(/* value */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  enqueue(value) {
+    let node = new ListNode(value);
+    if (this.size === 0){
+      this.firstEl = node;
+      this.lastEl = node;
+      this.size = 1;
+    } else {
+      this.lastEl.next = node;
+      this.lastEl = this.lastEl.next;
+      this.size++;
+    }
+
+    return this.firstEl;
   }
 
   dequeue() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    if (this.firstEl === null){
+      return null;
+    } else {
+      let elForDeletionVal = this.firstEl.value;
+      if (this.firstEl === this.lastEl) this.lastEl === null;
+      this.firstEl = this.firstEl.next;
+      this.size--;
+      return elForDeletionVal;
+    }
+    
+  }
+
+  getUnderlyingList() {
+    return this.firstEl;
   }
 
 }
